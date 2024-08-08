24e08a5ecb30f881b073c75fdea5ee661e012639
Shakeel <shakeeldvd23@gmail.com>
2024-08-08 09:02:12 +0200

    Middleware Authentication

---
+++ middleware/AuthenticateUser.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import jwt from 'jsonwebtoken'
+const { sign, verify } = jwt
+
+function createToken(user) {
+    return sign(
+        {
+            emailAdd: user.emailAdd,
+            pwd: user.pwd
+        },
+        process.env.SECRET_KEY,
+        {
+            expiresIn: '1h'
+        }
+    )
+}
